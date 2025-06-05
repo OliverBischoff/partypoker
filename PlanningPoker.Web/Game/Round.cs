c14dbb3a25146df43e74651295c8b0be699b76bf
oliver_bischoff <bischoff_oliver@gmx.de>
2025-06-05 13:18:27 +0200

    show more card information by reveal (count by weight, highest, lowest )

--- PlanningPoker.Web/Game/Round.cs
+++ PlanningPoker.Web/Game/Round.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PlanningPoker.Web.Game
 {
@@ -8,5 +9,26 @@ namespace PlanningPoker.Web.Game
         public Dictionary<Player, string> Cards { get; set; } = new Dictionary<Player, string>();
 
         public bool IsRevealed { get; set; } = false;
+
+        public List<(Player Player, string Card)> HighestCardsWithPlayer
+        {
+            get
+            {
+                var parsedCardValues = Cards.Select(c => (player: c.Key, card: double.Parse(c.Value)));
+
+                return [.. parsedCardValues.Where(p => p.card == parsedCardValues.Max(c => c.card)).Select(max => (max.player, max.card.ToString()))];
+            }
+        }
+
+        public List<(Player Player, string Card)> LowestCardsWithPlayer
+        {
+            get
+            {
+                var parsedCardValues = Cards.Select(c => (player: c.Key, card: double.Parse(c.Value)))
+                    .Where(p => !double.IsNaN(p.card));
+
+                return [.. parsedCardValues.Where(p => p.card == parsedCardValues.Min(c => c.card)).Select(max => (max.player, max.card.ToString()))];
+            }
+        }
     }
 }
